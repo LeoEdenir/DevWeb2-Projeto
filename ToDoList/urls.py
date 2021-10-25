9657c0d0a0f84f968f42736b09eb56e185d88d80
Leonardo Graciano <lgraciano@raioss.net>
2021-10-25 10:31:30 -0300

    adicionar viewsets de usuário e categoria

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from django.urls import include
+
+from core import views
+
+router = DefaultRouter()
+router.register('usuarios', views.UsuarioViewSet)
+router.register('categorias', views.CategoriaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
