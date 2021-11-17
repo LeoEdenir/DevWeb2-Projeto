648d80b73202765ce7e30948f8364ab3c1477c19
Leonardo Graciano <lgraciano@raioss.net>
2021-11-17 09:41:20 -0300

    adicionar autorização e autenticação

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from django.urls import include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from core import views
 
@@ -13,5 +17,7 @@ router.register('execucao-tarefa', views.ExecucaoTarefaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Categoria)
+admin.site.register(models.ExecucaoTarefa)
 
 
 class ItensInline(admin.TabularInline):
